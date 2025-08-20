9a4d4c111fbd0bf10daa153b0f69044a598e3587
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-08-20 11:54:34 +0600

    Add solution for Problem 10 - PingPong challenge

--- JavaScript-Problem-Solving.js
+++ JavaScript-Problem-Solving.js
@@ -97,3 +97,19 @@ function factorial(n) {
   return result;
 }
 console.log(factorial(5)); // Output: 120
+
+// Problem 10: PingPong Challenge
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+pingPong(); // Output: 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …
