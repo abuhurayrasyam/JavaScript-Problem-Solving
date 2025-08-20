23f9debaffee7e4de8e87b3edab5056d89d3e78b
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-08-20 11:44:11 +0600

    Add solution for Problem 4 - Find maximum number in array

--- JavaScript-Problem-Solving.js
+++ JavaScript-Problem-Solving.js
@@ -31,3 +31,15 @@ function isPalindrome(string) {
 }
 console.log(isPalindrome("madam")); // Output: true
 console.log(isPalindrome("hello")); // Output: false
+
+// Problem 4: Find the Maximum Number
+function findMax(array) {
+  let max = array[0];
+  for (let num of array) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
+}
+console.log(findMax([5, 1, 9, 3])); // Output: 9
