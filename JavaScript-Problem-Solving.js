2f46463e299ab05a8ea118dbaaebe315a67e978e
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-08-20 11:41:11 +0600

    Add solution for Problem 3 - Check if string is palindrome

--- JavaScript-Problem-Solving.js
+++ JavaScript-Problem-Solving.js
@@ -20,3 +20,14 @@ function countVowels(string) {
   return count;
 }
 console.log(countVowels("programming")); // Output: 3
+
+// Problem 3: Check for Palindrome
+function isPalindrome(string) {
+  let reversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return string === reversed;
+}
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
